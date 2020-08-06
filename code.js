a1490b376779a48a0c690bf15553e0352e5fa560
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-08-06 14:34:37 +0900

    add: ログイン機能を実装した

--- code.js
+++ code.js
@@ -3,7 +3,10 @@ function doGet(e) {
   if (page == "login" || page == null) {
     return HtmlService.createTemplateFromFile("login").evaluate();
   } else if (page == "mypage") {
-    return HtmlService.createTemplateFromFile("mypage").evaluate();
+    var template = HtmlService.createTemplateFromFile("mypage");
+    var id = e.parameter.id;
+    template.id = id;
+    return template.evaluate();
   } else if (page == "event") {
     return HtmlService.createTemplateFromFile("event").evaluate();
   } else if (page == "signup") {
@@ -14,4 +17,25 @@ function doGet(e) {
 function include(file) {
   return HtmlService
     .createHtmlOutputFromFile(file).getContent();
+}
+
+function loginCheck_gs(loginId, loginPassword) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("loginCheck");
+  for (let i = 2; i <= sheet.getLastRow(); i++) {
+    if (loginId === sheet.getRange(i, 1).getValue()) {
+      if (loginPassword === sheet.getRange(i, 2).getValue()) {
+        var userData = sheet.getRange(i, 1, 1, 3).getValues();
+        userData.push(getScriptUrl(2) + "&id=" + userData[0][1])
+        sheet.getRange(i, 4).setValue(userData[1]);
+        return userData;
+      }
+    }
+  }
+  throw "IDまたはパスワードが誤りです";
+}
+
+function getScriptUrl(i) {
+  const URL = ScriptApp.getService().getUrl();
+  const page = ["signup", "login", "mypage", "event",];
+  return URL + "?p=" + page[i];
 }
