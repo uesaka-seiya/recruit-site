639188a322399881fe64eb22e22d47cd9ed986ab
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-08-06 17:55:56 +0900

    add: イベント一覧表にテーブルから取得した値を表示した

--- code.js
+++ code.js
@@ -1,17 +1,14 @@
+const ss = SpreadsheetApp.getActiveSpreadsheet();
+const users = ss.getSheetByName("loginCheck");
+
 function doGet(e) {
-  var page = e.parameter["p"];
-  if (page == "login" || page == null) {
-    return HtmlService.createTemplateFromFile("login").evaluate();
-  } else if (page == "mypage") {
-    var template = HtmlService.createTemplateFromFile("mypage");
-    var id = e.parameter.id;
-    template.id = id;
-    return template.evaluate();
-  } else if (page == "event") {
-    return HtmlService.createTemplateFromFile("event").evaluate();
-  } else if (page == "signup") {
-    return HtmlService.createTemplateFromFile("signup").evaluate();
+  let page = e.parameter["p"];
+  if (page == null) {
+    page = "login";
   }
+  let template = HtmlService.createTemplateFromFile(page);
+  template.id = e.parameter.id;
+  return template.evaluate();
 }
 
 function include(file) {
@@ -19,29 +16,24 @@ function include(file) {
     .createHtmlOutputFromFile(file).getContent();
 }
 
-function loginCheck_gs(loginId, loginPassword) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("loginCheck");
-  for (let i = 2; i <= sheet.getLastRow(); i++) {
-    if (loginId === sheet.getRange(i, 1).getValue()) {
-      if (loginPassword === sheet.getRange(i, 2).getValue()) {
-        return getScriptUrl(2) + "&id=" + sheet.getRange(i, 1).getValue();
-      }
+function loginCheck_gs(id, password) {
+  for (let i = 2; i <= users.getLastRow(); i++) {
+    if (id === users.getRange(i, 1).getValue() && password === users.getRange(i, 2).getValue()) {
+      return getScriptUrl(2) + "&id=" + users.getRange(i, 1).getValue();
     }
   }
   throw "IDまたはパスワードが誤りです";
 }
 
 function getUserName(id) {
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("loginCheck");
-  for (let i = 2; i <= sheet.getLastRow(); i++) {
-    if (id === sheet.getRange(i, 1).getValue()) {
-      return sheet.getRange(i, 3).getValue();
+  for (let i = 2; i <= users.getLastRow(); i++) {
+    if (id === users.getRange(i, 1).getValue()) {
+      return users.getRange(i, 3).getValue();
     }
   }
 }
 
 function getScriptUrl(i) {
-  const URL = ScriptApp.getService().getUrl();
-  const page = ["signup", "login", "mypage", "event",];
-  return URL + "?p=" + page[i];
+  const page = ["signup", "login", "mypage", "event"];
+  return ScriptApp.getService().getUrl() + "?p=" + page[i];
 }
