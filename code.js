57d233c08e6202a29737d6f5f009f54612df94f5
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-08-12 15:44:46 +0900

    add: 新規ユーザー情報をDBに登録できるようにした

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 const ss = SpreadsheetApp.getActiveSpreadsheet();
-const users = ss.getSheetByName("loginCheck");
+const usersSheet = ss.getSheetByName("users");
 
 function doGet(e) {
   let page = e.parameter["p"];
@@ -7,7 +7,7 @@ function doGet(e) {
     page = "login";
   }
   let template = HtmlService.createTemplateFromFile(page);
-  template.id = e.parameter.id;
+  template.userId = e.parameter.id;
   return template.evaluate();
 }
 
@@ -16,19 +16,33 @@ function include(file) {
     .createHtmlOutputFromFile(file).getContent();
 }
 
+function setUserData(id, password, name, address, tel, school) {
+  for (let i = 2; i <= usersSheet.getLastRow(); i++) {
+    if (id == usersSheet.getRange(i, 1).getValue()) {
+      throw "このIDはすでに使われています。別のIDを再入力してください。";
+    } else {
+      const user = [[id, password, name, address, tel, school]];
+      usersSheet.getRange(usersSheet.getLastRow() + 1, 1, 1, 6).setValues(user);
+      console.log(usersSheet.getRange(1, usersSheet.getLastRow(), 1, 6).getValues());
+      const result = [getScriptUrl(2) + "&id=" + id, "登録が完了しました"];
+      return (result);
+    }
+  }
+}
+
 function loginCheck_gs(id, password) {
-  for (let i = 2; i <= users.getLastRow(); i++) {
-    if (id === users.getRange(i, 1).getValue() && password === users.getRange(i, 2).getValue()) {
-      return getScriptUrl(2) + "&id=" + users.getRange(i, 1).getValue();
+  for (let i = 2; i <= usersSheet.getLastRow(); i++) {
+    if (id === usersSheet.getRange(i, 1).getValue() && password === usersSheet.getRange(i, 2).getValue()) {
+      return getScriptUrl(2) + "&id=" + usersSheet.getRange(i, 1).getValue();
     }
   }
   throw "IDまたはパスワードが誤りです";
 }
 
 function getUserName(id) {
-  for (let i = 2; i <= users.getLastRow(); i++) {
-    if (id === users.getRange(i, 1).getValue()) {
-      return users.getRange(i, 3).getValue();
+  for (let i = 2; i <= usersSheet.getLastRow(); i++) {
+    if (id === usersSheet.getRange(i, 1).getValue()) {
+      return usersSheet.getRange(i, 3).getValue();
     }
   }
 }
