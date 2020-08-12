53d2a126182e8cd9ae99dc4b0070210691d5a0e2
uesaka-seiya <seiya.uesaka@esm.co.jp>
2020-08-12 14:33:08 +0900

    chore: メソッドの説明を追加した

--- code.js
+++ code.js
@@ -21,6 +21,14 @@ function include(file) {
     .createHtmlOutputFromFile(file).getContent();
 }
 
+/**
+ * IDとパスワードが一致するとマイページのURLを返す
+ *
+ * @param {*} id
+ * @param {*} password
+ * @return {*} マイページのURL
+ * @throws IDとパスワードが一致しないときにメッセージを投げる
+ */
 function loginCheck_gs(id, password) {
   for (let i = 2; i <= usersSheet.getLastRow(); i++) {
     if (id === usersSheet.getRange(i, 1).getValue() && password === usersSheet.getRange(i, 2).getValue()) {
@@ -30,6 +38,11 @@ function loginCheck_gs(id, password) {
   throw "IDまたはパスワードが誤りです";
 }
 
+/**
+ * ユーザーのIDから氏名を取得する
+ * @param {*} id
+ * @return {*} 氏名
+ */
 function getUserName(id) {
   for (let i = 2; i <= usersSheet.getLastRow(); i++) {
     if (id === usersSheet.getRange(i, 1).getValue()) {
@@ -38,11 +51,23 @@ function getUserName(id) {
   }
 }
 
+/**
+ * 遷移先URLを返す
+ *
+ * @param {*} i
+ * @return 遷移先URL
+ */
 function getScriptUrl(i) {
   const page = ["signup", "login", "mypage", "event"];
   return ScriptApp.getService().getUrl() + "?p=" + page[i];
 }
 
+/**
+ * 申込状況に応じて申込/キャンセルボタンのdisabled属性値を返す
+ *
+ * @param {*} userId
+ * @param {*} eventId
+ */
 function getStatus(userId, eventId) {
   var entryDisabled = false;
   var cancelDisabled = true;
@@ -57,6 +82,13 @@ function getStatus(userId, eventId) {
   return disabledStatus;
 }
 
+/**
+ * 申込ボタンを押した日時、イベントID、ユーザーIDをテーブルにセットする
+ *
+ * @param {*} userId
+ * @param {*} eventId
+ * @return 押されたボタンが配置されている行番号（ボタンのidと同値）
+ */
 function setEntry(userId, eventId) {
   entryStatusSheet.getRange(entryLastRow + 1, 1).setValue(new Date());
   entryStatusSheet.getRange(entryLastRow + 1, 2).setValue(eventId);
@@ -64,6 +96,13 @@ function setEntry(userId, eventId) {
   return eventId;
 }
 
+/**
+ * イベントID、ユーザーIDが一致する申込ログレコードを削除する
+ *
+ * @param {*} userId
+ * @param {*} eventId
+ * @return 押されたボタンが配置されている行番号（ボタンのidと同値）
+ */
 function deleteEntry(userId, eventId) {
   for (let i = 2; i <= entryLastRow; i++) {
     if (userId == entryStatusSheet.getRange(i, 3).getValue() && eventId == entryStatusSheet.getRange(i, 2).getValue()) {
